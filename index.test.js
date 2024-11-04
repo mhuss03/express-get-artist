b4fa017b36f9e926e96b17d1b9dc018742a32c9f
mhuss03 <134786667+mhuss03@users.noreply.github.com>
2024-11-04 14:44:38 +0000

    Completed

--- index.test.js
+++ index.test.js
@@ -1,22 +1,18 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
-const {seedMusician} = require("./seedData");
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
+const { seedMusician } = require("./seedData");
 
-
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+describe("./musicians endpoint", () => {
+  // Write your tests here
+  test("Testing returns All Musicians", async () => {
+    const res = await request(app).get("/musicians");
+    expect(res.statusCode).toBe(200);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
-
-
-
-
-
-
+//TODO: Create a GET /musicians route to return all musicians
+app.get("/musicians", async (req, res) => {
+  const allMusicians = await Musician.findAll();
+  res.send(allMusicians);
+});
 
 module.exports = app;
